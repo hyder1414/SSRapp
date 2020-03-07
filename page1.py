c8f70beb3ca6fb878ece81d5ad187b753f4f2526
M. Haider Khan <34618387+hyder1414@users.noreply.github.com>
2020-03-07 12:11:35 -0600

    after deleting the sample and making the first window..HK

--- page1.py
+++ page1.py
@@ -1,25 +1,5 @@
-import tkinter as Case Assign
+from tkinter import *
+root = Tk()
+root.title("SSRapp")
 
-class Application(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.master = master
-        self.pack()
-        self.create_widgets()
-
-    def create_widgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
-
-        self.quit = tk.Button(self, text="QUIT", fg="red",
-                              command=self.master.destroy)
-        self.quit.pack(side="bottom")
-
-    def say_hi(self):
-        print("hi there, everyone!")
-
-root = tk.Tk()
-app = Application(master=root)
-app.mainloop()
+root.mainloop()
